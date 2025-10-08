5895087cbc4e929c222c6a069336c82888f2850e
stern9 <stern9@gmail.com>
2025-10-08 15:12:53 -0600

    Add dotenv support and fix login navigation timeout

--- scripts/reserve.js
+++ scripts/reserve.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
+require("dotenv").config();
 
 // ============================================================================
 // CONFIGURATION
