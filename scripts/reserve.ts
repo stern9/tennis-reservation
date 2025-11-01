5566a786922cad65b89cb5d0a01ca0b471105576
stern9 <stern9@gmail.com>
2025-11-01 17:14:19 -0600

    missed court 1 monday, try court 2

--- scripts/reserve.ts
+++ scripts/reserve.ts
@@ -71,6 +71,7 @@ const CONFIG: AppConfig = {
       name: "Cancha de Tenis 2",
       daysAhead: 8, // Court 2 becomes available 8 days in advance
       slots: {
+        Monday: "06:00 AM - 07:00 AM",
         Tuesday: "07:00 AM - 08:00 AM",
         Friday: "07:00 AM - 08:00 AM",
       },
